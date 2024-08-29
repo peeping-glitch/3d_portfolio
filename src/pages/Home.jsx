7048268339a86b18ef01402309410dac8bde0d88
diego vera <diegovera2777@gmail.com>
2024-08-29 12:57:05 -0500

    new file:   .gitignore 	new file:   README.md 	new file:   eslint.config.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/vite.svg 	new file:   src/App.jsx 	new file:   src/assets/3d/bird.glb 	new file:   src/assets/3d/fox.glb 	new file:   src/assets/3d/island.glb 	new file:   src/assets/3d/plane.glb 	new file:   src/assets/3d/sky.glb 	new file:   src/assets/favicon.ico 	new file:   src/assets/icons/arrow.svg 	new file:   src/assets/icons/car.svg 	new file:   src/assets/icons/contact.svg 	new file:   src/assets/icons/css.svg 	new file:   src/assets/icons/estate.svg 	new file:   src/assets/icons/express.svg 	new file:   src/assets/icons/git.svg 	new file:   src/assets/icons/github.svg 	new file:   src/assets/icons/html.svg 	new file:   src/assets/icons/index.js 	new file:   src/assets/icons/javascript.svg 	new file:   src/assets/icons/linkedin.svg 	new file:   src/assets/icons/mongodb.svg 	new file:   src/assets/icons/motion.svg 	new file:   src/assets/icons/mui.svg 	new file:   src/assets/icons/nextjs.svg 	new file:   src/assets/icons/nodejs.svg 	new file:   src/assets/icons/pricewise.svg 	new file:   src/assets/icons/react.svg 	new file:   src/assets/icons/redux.svg 	new file:   src/assets/icons/sass.svg 	new file:   src/assets/icons/snapgram.svg 	new file:   src/assets/icons/soundoff.png 	new file:   src/assets/icons/soundon.png 	new file:   src/assets/icons/summiz.svg 	new file:   src/assets/icons/tailwindcss.svg 	new file:   src/assets/icons/threads.svg 	new file:   src/assets/icons/twitter.svg 	new file:   src/assets/icons/typescript.svg 	new file:   src/assets/images/hero.jpg 	new file:   src/assets/images/index.js 	new file:   src/assets/images/logo.svg 	new file:   src/assets/images/meta.png 	new file:   src/assets/images/shopify.png 	new file:   src/assets/images/starbucks.png 	new file:   src/assets/images/tesla.png 	new file:   src/assets/react.svg 	new file:   src/assets/sakura.mp3 	new file:   src/components/Loader.jsx 	new file:   src/components/Navbar.jsx 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   src/models/Island.jsx 	new file:   src/pages/About.jsx 	new file:   src/pages/Contacts.jsx 	new file:   src/pages/Home.jsx 	new file:   src/pages/Index.jsx 	new file:   src/pages/Projects.jsx 	new file:   tailwind.config.js 	new file:   vite.config.js

---
+++ src/pages/Home.jsx
@@ -0,0 +1,52 @@
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
+
+import Island from '../models/Island';
+
+{/* <div className="absolute top-28 left-0 right-0 z-10
+items-center justify-center">
+  POPUP
+</div> */}
+
+const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43 ];
+    let rotation = [0.1, 4.7,0];
+
+    if(window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+  } else {
+    screenScale = [1, 1, 1];
+  }
+    return [screenScale, screenPosition, rotation];
+  }
+
+  const [ islandScale, islandPosition, rotation ] = adjustIslandForScreenSize();
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas className="w-full h-screen 
+      bg-transparent"
+      camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1,1,1]} intensity={2} />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+
+          <Island
+            scale={islandScale} 
+            position={islandPosition}
+            rotation={rotation} 
+            />
+        </Suspense>
+
+      </Canvas>
+    </section>
+  )
+}
+
+export default Home
